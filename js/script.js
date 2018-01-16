c297db83cb63558a385240748d6ebf17454af0bc
Fernando Sakabe <fernandokenjisakabe@gmail.com>
2018-01-16 11:35:28 -0200

    jogo de advinha

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+/*
+Jogo de advinha
+
+O usuário tenta adivinhar um número aleatório entre 1 e 10.
+
+Criado por: fernando Sakabe
+11985555871
+contato: fernandokenjisakabe@gmail.com
+
+*/
+
+let numero = 0;
+const numeroSecreto = Math.floor(Math.random()*10)+1;
+let tentativas = 3;
+
+while((numero!==numeroSecreto) && (tentativas>0)){
+
+let numero = Number(prompt("Advinhe o numero de 1 a 10:"));
+tentativas=tentativas-1;
+
+if(numero=== numeroSecreto){
+  alert("O número que você inseriu é o correto ");
+  tentativas=0;
+    }
+else{
+  alert("O número que você inseriu é o errado, você temmais " +tentativas + " tentativas");
+
+    }
+}
